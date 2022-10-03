1f8acab7f97d39405a7ecce55de7c5b558efb2be
norbert <norbert.corba@gmail.com>
2022-10-04 00:12:56 +0200

    fourth commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,33 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>
+        @yield('title')
+    </title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sticky-footer-navbar/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+
+   <body>
+    <div class="container">
+        <nav class="nav">
+          <a class="nav-link" href="/movies">Movies</a>
+          <a class="nav-link" href="/add">Add Movie</a>
+        </nav>
+      </div>
+      @yield('content')
+  </body>
+</html>
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,14 +1,20 @@
+@extends('layouts.master')
+
+@section('title', 'Movies')
+
+@section('content')
+
+
 @foreach($movies as $movie)
 
 <h1> 
-    <a href="/singlemovie">{{ $movie->Title}}</a></h1>   
+    <a href="/singlemovie">{{ $movie->Title}}</a>
+</h1>   
 
     <h2> 
         {{ $movie->Storyline }}
     </h2>   
- <h1>   
-   
-
-</h1>
 
 @endforeach
+    
+@endsection
